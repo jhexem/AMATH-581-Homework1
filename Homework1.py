01e79e9407a7c04a6de9fd01e3599345312db03b
Justin Hexem <justinbhexem@icloud.com>
2022-10-17 16:14:10 -0700

    rough draft 2

--- Homework1.py
+++ Homework1.py
@@ -9,7 +9,7 @@ def ytrue(t):     #defines ytrue
    return (np.pi * np.exp(3 * (np.cos(t) - 1)) / np.sqrt(2))
 
 t0 = 0      #initializes t0, y0, the array of all dt values, and the ytrue value at t=5
-y0 = np.pi / 2
+y0 = np.pi / np.sqrt(2)
 dt = np.array([2**(-2), 2**(-3), 2**(-4), 2**(-5), 2**(-6), 2**(-7), 2**(-8)])
 ans = ytrue(5)
 
@@ -18,14 +18,14 @@ def forward_euler(t0, y0, dt, dydt, ans):    #returns a tuple of the y value for
 
    for j in range(len(dt)):      #iterates through each dt value
       dtvals = np.arange(0, 5 + dt[j], dt[j])      #creates an array of t values each spaced apart from the previous value by dt
-      yvals = np.array(y0)
 
       for i in range(len(dtvals)):     #iterates thru the list of dt values and calculates the next y value using FE
          if i == 0:
             y = y0 + dt[j] * dydt(t0, y0)
+            yvals = np.array(y0)
          else:
             y = y + dt[j] * dydt(dtvals[i], y)
-         yvals = np.append(yvals, y)
+            yvals = np.append(yvals, y)
 
       errorlist[j] = np.abs(ans - y)      #adds the error for this value of dt to the error list
       
@@ -38,12 +38,12 @@ A2 = np.array([solfe[1]])
 coefficientsfe = np.polyfit(np.log(dt), np.log(solfe[1]), 1)
 A3 = coefficientsfe[0]
 
-xaxis = np.linspace(0.001, 1, 1000)
+xaxis = np.linspace(0.0001, 1, 1000)
 '''plt.loglog(dt, solfe[1], '.r')
 coefficientsfe = np.polyfit(np.log(dt), np.log(solfe[1]), 1)
 polynomialfe = np.poly1d(coefficientsfe)
 ylinefe = polynomialfe(np.log(xaxis))
-plt.plot(xaxis, np.exp(yline), '-b')
+plt.plot(xaxis, np.exp(ylinefe), '-b')
 plt.show()'''
 
 def heun(t0, y0, dt, dydt, ans):
@@ -51,14 +51,14 @@ def heun(t0, y0, dt, dydt, ans):
 
    for j in range(len(dt)):
       dtvals = np.arange(0, 5 + dt[j], dt[j])
-      yvals = np.array([y0])
 
       for i in range(len(dtvals)):
          if i == 0:
             y = y0 + (dt[j] / 2) * (dydt(t0, y0) + dydt(t0 + dt[j], y0 + dt[j] * dydt(t0, y0)))
+            yvals = np.array(y0)
          else:
             y = y + (dt[j] / 2) * (dydt(dtvals[i], y) + dydt(dtvals[i] + dt[j], y + dt[j] * dydt(dtvals[i], y)))
-         yvals = np.append(yvals, y)
+            yvals = np.append(yvals, y)
 
       errorlist[j] = np.abs(ans - y)
 
@@ -71,13 +71,11 @@ A5 = np.array([solheun[1]])
 coefficientsheun = np.polyfit(np.log(dt), np.log(solheun[1]), 1)
 A6 = coefficientsheun[0]
 
-
 '''plt.loglog(dt, solheun[1], '.r')
 polynomialheun = np.poly1d(coefficientsheun)
 ylineheun = polynomialheun(np.log(xaxis))
 plt.plot(xaxis, np.exp(ylineheun), '-b')
-plt.show()
-A6 = coefficientsheun[1]'''
+plt.show()'''
 
 def RK2(t0, y0, dtval, dydt):
    return y0 + dtval * dydt(t0 + (dtval/2), y0 + (dtval/2) * dydt(t0, y0))
@@ -87,16 +85,17 @@ def adams(t0, y0, dt, dydt, ans):
 
    for j in range(len(dt)):
       dtvals = np.arange(0, 5 + dt[j], dt[j])
-      yvals = np.array([y0])
 
       for i in range(len(dtvals)):
          if i == 0:
             yprev = y0
             y = RK2(t0, y0, dt[j], dydt)
+            yvals = np.array([y0])
          else:
-            ypred = y + (dt[j]/2) * (3*dydt(dtvals[i], y) - dydt(dtvals[i-1], yprev))
+            ypred = y + (dt[j]/2) * (3 * dydt(dtvals[i], y) - dydt(dtvals[i-1], yprev))
+            yprev = y
             y = y + (dt[j]/2) * (dydt(dtvals[i] + dt[j], ypred) + dydt(dtvals[i], y))
-         yvals = np.append(yvals, y)
+            yvals = np.append(yvals, y)
 
       errorlist[j] = np.abs(y - ans)
 
@@ -130,7 +129,7 @@ tlist = np.arange(0, 32 + dt, dt)
 def dzdte1(t, z):
    w1 = z[0]
    w2 = z[1]
-   return np.array([w2, (-0.1) * ((w1 ** 2) - 1) * w2 + w1])
+   return np.array([w2, (-0.1) * ((w1 ** 2) - 1) * w2 - w1])
 
 ep1 = solve_ivp(dzdte1, [0, 32], z0, method='RK45', t_eval=tlist)
 ep1col = np.transpose(np.array([ep1.y[0]]))
@@ -138,7 +137,7 @@ ep1col = np.transpose(np.array([ep1.y[0]]))
 def dzdte2(t, z):
    w1 = z[0]
    w2 = z[1]
-   return np.array([w2, (-1) * ((w1 ** 2) - 1) * w2 + w1])
+   return np.array([w2, (-1) * ((w1 ** 2) - 1) * w2 - w1])
 
 ep2 = solve_ivp(dzdte2, [0, 32], z0, method='RK45', t_eval=tlist)
 ep2col = np.transpose(np.array([ep2.y[0]]))
@@ -146,7 +145,7 @@ ep2col = np.transpose(np.array([ep2.y[0]]))
 def dzdte3(t, z):
    w1 = z[0]
    w2 = z[1]
-   return np.array([w2, (-20) * ((w1 ** 2) - 1) * w2 + w1])
+   return np.array([w2, (-20) * ((w1 ** 2) - 1) * w2 - w1])
 
 ep3 = solve_ivp(dzdte3, [0, 32], z0, method='RK45', t_eval=tlist)
 ep3col = np.transpose(np.array([ep3.y[0]]))
@@ -197,48 +196,48 @@ A13 = coefficientBDF[0]
 y0 = np.array([0.1, 0, 0.1, 0])
 
 def sys_rhs1(t, y):
-   v1, w1, v2, w2 = y
-   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 + 0 * v2, 0.1 * v1 - 0.1 * w1, 
-   - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0 * v1, 0.1 * v2 - 0.1 * w2]
+   v1, v2, w1, w2 = y
+   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 + 0 * v2, - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0 * v1, 
+   0.1 * v1 - 0.1 * w1, 0.1 * v2 - 0.1 * w2]
    return  dydt
 
 def sys_rhs2(t, y):
-   v1, w1, v2, w2 = y
-   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 + 0 * v2, 0.1 * v1 - 0.1 * w1, 
-   - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 0.1 * v2 - 0.1 * w2]
+   v1, v2, w1, w2 = y
+   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 + 0 * v2, - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 
+   0.1 * v1 - 0.1 * w1, 0.1 * v2 - 0.1 * w2]
    return  dydt
 
 def sys_rhs3(t, y):
-   v1, w1, v2, w2 = y
-   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 - 0.1 * v2, 0.1 * v1 - 0.1 * w1, 
-   - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 0.1 * v2 - 0.1 * w2]
+   v1, v2, w1, w2 = y
+   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 - 0.1 * v2, - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 
+   0.1 * v1 - 0.1 * w1, 0.1 * v2 - 0.1 * w2]
    return  dydt
 
 def sys_rhs4(t, y):
-   v1, w1, v2, w2 = y
-   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 - 0.3 * v2, 0.1 * v1 - 0.1 * w1, 
-   - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 0.1 * v2 - 0.1 * w2]
+   v1, v2, w1, w2 = y
+   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 - 0.3 * v2, - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 
+   0.1 * v1 - 0.1 * w1, 0.1 * v2 - 0.1 * w2]
    return  dydt
 
 def sys_rhs5(t, y):
-   v1, w1, v2, w2 = y
-   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 - 0.5 * v2, 0.1 * v1 - 0.1 * w1, 
-   - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 0.1 * v2 - 0.1 * w2]
+   v1, v2, w1, w2 = y
+   dydt = [- v1 ** 3 + (1 + 0.05) * (v1 ** 2) - 0.05 * v1 - w1 + 0.1 - 0.5 * v2, - v2 ** 3 + (1 + 0.25) * (v2 ** 2) - 0.25 * v2 - w2 + 0.1 + 0.2 * v1, 
+   0.1 * v1 - 0.1 * w1, 0.1 * v2 - 0.1 * w2]
    return  dydt
 
-tlist = np.arange(0, 100 + 0.5, 0.5)
+tlist2 = np.arange(0, 100 + 0.5, 0.5)
 
-solFH1 = solve_ivp(sys_rhs1, [0, 100], y0, method='BDF', t_eval=tlist)
+solFH1 = solve_ivp(sys_rhs1, [0, 100], y0, method='BDF', t_eval=tlist2)
 A14 = np.transpose(np.array(solFH1.y))
 
-solFH2 = solve_ivp(sys_rhs2, [0, 100], y0, method='BDF', t_eval=tlist)
+solFH2 = solve_ivp(sys_rhs2, [0, 100], y0, method='BDF', t_eval=tlist2)
 A15 = np.transpose(np.array(solFH2.y))
 
-solFH3 = solve_ivp(sys_rhs3, [0, 100], y0, method='BDF', t_eval=tlist)
+solFH3 = solve_ivp(sys_rhs3, [0, 100], y0, method='BDF', t_eval=tlist2)
 A16 = np.transpose(np.array(solFH3.y))
 
-solFH4 = solve_ivp(sys_rhs4, [0, 100], y0, method='BDF', t_eval=tlist)
+solFH4 = solve_ivp(sys_rhs4, [0, 100], y0, method='BDF', t_eval=tlist2)
 A17 = np.transpose(np.array(solFH4.y))
 
-solFH5 = solve_ivp(sys_rhs5, [0, 100], y0, method='BDF', t_eval=tlist)
+solFH5 = solve_ivp(sys_rhs5, [0, 100], y0, method='BDF', t_eval=tlist2)
 A18 = np.transpose(np.array(solFH5.y))
